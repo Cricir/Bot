c631a3ff50b5d55db9659db17b865671b6972f2f
Cricir <sidelwalk@gmail.com>
2024-02-14 16:31:05 +0100

    ParaMasBonito

--- bot.js
+++ bot.js
@@ -30,18 +30,18 @@ const bot = new TelegramBot(token, { polling: true });
 * @param {Object} msg - El objeto de mensaje recibido.
 */
 bot.onText(/\/start/, (msg) => {
-const chatId = msg.chat.id;
-const options = {
-reply_markup: {
-keyboard: [
-['/clima', '/guardar'],
-['/recuperar']
-],
-resize_keyboard: true,
-one_time_keyboard: true
-}
-};
-bot.sendMessage(chatId, 'Hola, soy un bot de Telegram!', options);
+    const chatId = msg.chat.id;
+    const options = {
+        reply_markup: {
+            keyboard: [
+                ['/clima', '/guardar'],
+                ['/recuperar']
+            ],
+            resize_keyboard: true,
+            one_time_keyboard: true
+        }
+    };
+    bot.sendMessage(chatId, 'Hola, soy un bot de Telegram!', options);
 });
 
 /**
@@ -49,8 +49,8 @@ bot.sendMessage(chatId, 'Hola, soy un bot de Telegram!', options);
 * @param {Object} msg - El objeto de mensaje recibido.
 */
 bot.on('photo', (msg) => {
-const chatId = msg.chat.id;
-bot.sendMessage(chatId, '¡Bonita imagen!');
+    const chatId = msg.chat.id;
+    bot.sendMessage(chatId, '¡Bonita imagen!');
 });
 
 const axios = require('axios');
@@ -63,17 +63,17 @@ const api_key = 'ab926ae7c724437426c28dbf2bf11078';
 * @param {Array} match - Los resultados de la coincidencia de la expresión regular.
 */
 bot.onText(/\/clima (.+)/, async (msg, match) => {
-const chatId = msg.chat.id;
-const ciudad = match[1];
+    const chatId = msg.chat.id;
+    const ciudad = match[1];
 
-try {
-const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${ciudad}&appid=${api_key}`);
-const clima = response.data;
-bot.sendMessage(chatId, `El clima actual en ${ciudad} es: ${clima.weather[0].description}`);
-} catch (error) {
-console.error('Error al obtener el clima:', error);
-bot.sendMessage(chatId, 'Lo siento, no pude obtener el clima en este momento.');
-}
+    try {
+        const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${ciudad}&appid=${api_key}`);
+        const clima = response.data;
+        bot.sendMessage(chatId, `El clima actual en ${ciudad} es: ${clima.weather[0].description}`);
+    } catch (error) {
+        console.error('Error al obtener el clima:', error);
+        bot.sendMessage(chatId, 'Lo siento, no pude obtener el clima en este momento.');
+    }
 });
 
 /**
@@ -81,8 +81,8 @@ bot.sendMessage(chatId, 'Lo siento, no pude obtener el clima en este momento.');
 * @param {Object} msg - El objeto de mensaje recibido.
 */
 bot.onText(/\/clima/, (msg) => {
-const chatId = msg.chat.id;
-const message = `
+    const chatId = msg.chat.id;
+    const message = `
 Para obtener el clima de una ciudad, simplemente escribe /clima seguido del nombre de la ciudad.
 
 Por ejemplo:
@@ -92,7 +92,7 @@ Por ejemplo:
 
 ¡Espero que esta información te sea útil!
 `;
-bot.sendMessage(chatId, message);
+    bot.sendMessage(chatId, message);
 });
 
 const Datastore = require('nedb');
@@ -108,18 +108,18 @@ const db = new Datastore({ filename: 'BaseDatos.db', autoload: true });
 * @param {Array} match - Los resultados de la coincidencia de la expresión regular.
 */
 bot.onText(/\/guardar (.+)/, (msg, match) => {
-const chatId = msg.chat.id;
-const data = match[1];
+    const chatId = msg.chat.id;
+    const data = match[1];
 
-// Guardar datos en la base de datos
-db.insert({ chatId, data }, (err, newDoc) => {
-if (err) {
-console.error('Error al guardar datos:', err);
-bot.sendMessage(chatId, 'Error al guardar datos.');
-} else {
-bot.sendMessage(chatId, 'Datos guardados correctamente.');
-}
-});
+    // Guardar datos en la base de datos
+    db.insert({ chatId, data }, (err, newDoc) => {
+        if (err) {
+            console.error('Error al guardar datos:', err);
+            bot.sendMessage(chatId, 'Error al guardar datos.');
+        } else {
+            bot.sendMessage(chatId, 'Datos guardados correctamente.');
+        }
+    });
 });
 
 /**
@@ -127,16 +127,16 @@ bot.sendMessage(chatId, 'Datos guardados correctamente.');
 * @param {Object} msg - El objeto de mensaje recibido.
 */
 bot.onText(/\/recuperar/, (msg) => {
-const chatId = msg.chat.id;
+    const chatId = msg.chat.id;
 
-// Recuperar datos de la base de datos
-db.find({ chatId }, (err, docs) => {
-if (err) {
-console.error('Error al recuperar datos:', err);
-bot.sendMessage(chatId, 'Error al recuperar datos.');
-} else {
-const data = docs.length > 0 ? docs[0].data : 'No hay datos guardados.';
-bot.sendMessage(chatId, 'Los datos son: ' + data);
-}
-});
+    // Recuperar datos de la base de datos
+    db.find({ chatId }, (err, docs) => {
+        if (err) {
+            console.error('Error al recuperar datos:', err);
+            bot.sendMessage(chatId, 'Error al recuperar datos.');
+        } else {
+            const data = docs.length > 0 ? docs[0].data : 'No hay datos guardados.';
+            bot.sendMessage(chatId, 'Los datos son: ' + data);
+        }
+    });
 });
